8018a822c5676cea0e5b86e333b710d82bc12348
Husnain Akhtar <husnainakhtar06@gmail.com>
2024-02-25 20:46:25 +0500

    initial commit

---
+++ app/(site)/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> Vizzify Web | Platform for Stunning Web Apps</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Built with Next.js and TypeScript" />
+      <link rel="icon" href="/images/logo/logo.png" />
+    </>
+  );
+}
